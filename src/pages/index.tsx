7e00a75265f0cae1d66809261f324e059d8a3429
dineshpinto <annual.fallout_0z@icloud.com>
2024-05-18 16:44:59 +0400

    Fix description, switch to node v20 deploys

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description={`${siteConfig.tagline}`}
     >
       <HomepageHeader />
       <main>
