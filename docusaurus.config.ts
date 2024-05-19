a605996d09f0a339cbdb452d66e679e258bdccc7
dineshpinto <annual.fallout_0z@icloud.com>
2024-05-19 12:49:04 +0400

    Subdivided navigation into navbar tabs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,135 +1,148 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Flare Developer Hub',
-  tagline: 'The decentralized origin for Flare builders. Written by builders, for builders.',
-  favicon: '/img/favicon.ico', // TODO: This should be an ICO file
+  title: "Flare Developer Hub",
+  tagline:
+    "The decentralized origin for Flare builders. Written by builders, for builders.",
+  favicon: "/img/favicon.ico", // TODO: This should be an ICO file
 
   // Set the production url of your site here
-  url: 'https://dineshpinto.github.io',
+  url: "https://dineshpinto.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/developer-hub/',
+  baseUrl: "/developer-hub/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dineshpinto', // Usually your GitHub org/user name.
-  projectName: 'developer-hub', // Usually your repo name.
+  organizationName: "dineshpinto", // Usually your GitHub org/user name.
+  projectName: "developer-hub", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: 'all',
+            type: "all",
             copyright: `Copyright © Flare Networks ${new Date().getFullYear()}.`,
           },
-          blogDescription: 'Guides',
-          blogSidebarCount: 'ALL',
-          blogSidebarTitle: 'All guides',
+          blogDescription: "Guides",
+          blogSidebarCount: "ALL",
+          blogSidebarTitle: "All guides",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    image: 'img/flare_tbfd_pink.jpg',
-    defaultMode: 'light',
+    image: "img/flare_tbfd_pink.jpg",
+    defaultMode: "light",
     respectPrefersColorScheme: true,
     navbar: {
-      title: 'Developer Hub',
+      title: "Developer Hub",
       hideOnScroll: false,
       logo: {
-        alt: 'Flare Icon',
-        src: 'img/flare_icon.svg',
+        alt: "Flare Icon",
+        src: "img/flare_icon.svg",
         width: 32,
         height: 32,
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "networkSidebar",
+          position: "left",
+          label: "Network",
         },
-        {to: '/blog', label: 'Guides', position: 'left'},
         {
-          href: 'https://github.com/dineshpinto/developer-hub',
-          label: 'GitHub',
-          position: 'right',
+          type: "docSidebar",
+          sidebarId: "dataProtocolsSidebar",
+          position: "left",
+          label: "Data Protocols",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "technicalReferenceSidebar",
+          position: "left",
+          label: "Technical Reference",
+        },
+        { to: "/blog", label: "Guides", position: "left" },
+        {
+          href: "https://github.com/dineshpinto/developer-hub",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       logo: {
-        alt: 'Flare Logo',
-        src: 'img/flare_tbfd_light.svg',
-        srcDark: 'img/flare_tbfd_dark.svg',
-        width: 300
+        alt: "Flare Logo",
+        src: "img/flare_tbfd_light.svg",
+        srcDark: "img/flare_tbfd_dark.svg",
+        width: 300,
       },
       links: [
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'X',
-              href: 'https://x.com/FlareNetworks',
+              label: "X",
+              href: "https://x.com/FlareNetworks",
             },
             {
-              label: 'Telegram',
-              href: 'https://t.me/FlareNetwork',
+              label: "Telegram",
+              href: "https://t.me/FlareNetwork",
             },
             {
-              label: 'Discord',
-              href: 'https://discord.com/invite/flarenetwork',
+              label: "Discord",
+              href: "https://discord.com/invite/flarenetwork",
             },
             {
-              label: 'LinkedIn',
-              href: 'https://www.linkedin.com/company/flarenetwork/',
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/company/flarenetwork/",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
             },
             {
-              label: 'YouTube',
-              href: 'https://www.youtube.com/channel/UCDyqyTWHYMWY5ie6xOCgG0w',
+              label: "YouTube",
+              href: "https://www.youtube.com/channel/UCDyqyTWHYMWY5ie6xOCgG0w",
             },
             {
-              label: 'Medium',
-              href: 'https://medium.com/flarenetwork',
+              label: "Medium",
+              href: "https://medium.com/flarenetwork",
             },
             {
-              label: 'Terms & Privacy',
-              href: 'https://flare.network/privacy-policy/',
+              label: "Terms & Privacy",
+              href: "https://flare.network/privacy-policy/",
             },
           ],
         },
@@ -137,7 +150,7 @@ const config: Config = {
       copyright: `Copyright © Flare Networks ${new Date().getFullYear()}.`,
     },
     prism: {
-      additionalLanguages: ['solidity', 'bash', 'json', 'toml'],
+      additionalLanguages: ["solidity", "bash", "json", "toml"],
       theme: prismThemes.oneDark,
       darkTheme: prismThemes.oneDark,
     },
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -63,7 +63,7 @@ function Feature({ title, Svg, description, link }: FeatureItem) {
 function FeatureCard({ title, Svg, description, link }: FeatureItem) {
   return (
     <div className={clsx("col col--4")}>
-      <div className="card margin--sm padding-bottom--sm">
+      <div className="card margin--sm padding--sm">
         <div className="text--center">
           <Svg className={styles.featureSvg} role="img" />
         </div>
