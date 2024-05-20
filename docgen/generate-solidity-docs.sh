1b6b44205feb8dec94d15769b9a47ae686d7e24c
dineshpinto <annual.fallout_0z@icloud.com>
2024-05-20 18:45:08 +0400

    Cleanup bash script

--- docgen/generate-solidity-docs.sh
+++ docgen/generate-solidity-docs.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 # See README.md for usage instructions.
-
+INPUT_PATH="docs/userInterfaces/"
+OUTPUT_PATH="../../docs/technical-reference/contracts/"
 set -e
 
 # Defined in repos.list
-while IFS=' ' read -r repo_url repo_branch repo_source_path hardhat_config_file build_command || [ -n "$build_command" ];
+while IFS=' ' read -r repo_url repo_branch repo_source_path build_command || [ -n "$build_command" ];
 do
     repo_name=$(basename $repo_url .git)
     rm -rf $repo_name
     git clone --depth 1 $repo_url
-
+    
     # Move to the repo
     cd $repo_name
     git checkout $repo_branch
-
+    
     # Install dependencies
     yarn
     yarn add solidity-docgen
-
+    
     # Patch hardhat.config.ts
-    sed -i -E "1s/^/import 'solidity-docgen';\n/" $hardhat_config_file
-    sed -i -E "/HardhatUserConfig = / r ../hardhat.config.ts.patch" $hardhat_config_file
-
+    sed -i -E "1s/^/import 'solidity-docgen';\n/" hardhat.config.ts
+    sed -i -E "/HardhatUserConfig = / r ../hardhat.config.ts.patch" hardhat.config.ts
+    
     # Copy template
     cp -r ../templates .
     
     # Build docs
     echo -e "\nBuilding docs for $repo_name"
     yarn hardhat docgen
-
+    
     # Copy docs
-    cp -r docs/userInterfaces/ ../../docs/technical-reference/contracts/
+    echo -e "\nCopying docs from $INPUT_PATH to $OUTPUT_PATH"
+    cp -r $INPUT_PATH $OUTPUT_PATH
 done < "repos.list"
