7903a9e3c8d58d4baede21d276654e9091ee72aa
dineshpinto <annual.fallout_0z@icloud.com>
2024-05-29 22:41:05 +0400

    Minor refactoring

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -16,8 +16,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require("@site/static/img/FTSO.svg").default,
     description: (
       <>
-        High-integrity, low-latency data feeds for decentralized applications on
-        Flare
+        High-integrity, block-latency data feeds for DeFi applications on Flare
       </>
     ),
     linkToProtocolDocs: "docs/ftso/overview",
@@ -37,28 +36,11 @@ const FeatureList: FeatureItem[] = [
     title: "FAssets",
     Svg: require("@site/static/img/FASSETS.svg").default,
     description: (
-      <>
-        Trust-minimized, decentralized data bridge for connecting Bitcoin to
-        Flare
-      </>
+      <>Trust-minimized, decentralized data bridge bringing Bitcoin to Flare</>
     ),
     linkToProtocolDocs: "docs/fassets/intro",
   },
 ];
-
-function Feature({ title, Svg, description, link }: FeatureItem) {
-  return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
 
 function FeatureCard({
   title,
