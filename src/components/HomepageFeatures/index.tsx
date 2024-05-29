5bed243f3d045738b8b51632e92c67e17b8b55e1
dineshpinto <annual.fallout_0z@icloud.com>
2024-05-29 21:23:57 +0400

    Add diagram to homepage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -31,7 +31,7 @@ const FeatureList: FeatureItem[] = [
         Flare
       </>
     ),
-    linkToProtocolDocs: "docs/fdc/intro",
+    linkToProtocolDocs: "docs/fdc/overview",
   },
   {
     title: "FAssets",
