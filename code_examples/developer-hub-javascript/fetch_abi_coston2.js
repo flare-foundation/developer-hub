6a9a8d9587b54e24a6a20a9ae72b2dfdb7d336ea
dineshpinto <annual.fallout_0z@icloud.com>
2024-06-02 00:41:29 +0400

    Add JavaScript guide

--- code_examples/developer-hub-javascript/fetch_abi_coston2.js
+++ code_examples/developer-hub-javascript/fetch_abi_coston2.js
@@ -3,5 +3,5 @@ const params =
   "?module=contract&action=getabi&address=0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019";
 
 const response = await fetch(base_url + params);
-const abi = (await response.json())["result"];
+const abi = JSON.parse((await response.json())["result"]);
 console.log(abi);
