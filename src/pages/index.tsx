e9cee5f6692820a9099d52b5d5a84a4929ad631a
dineshpinto <annual.fallout_0z@icloud.com>
2024-06-05 00:52:50 +0400

    Restructure url layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to="docs/flare/intro"
-          >
+          <Link className="button button--primary button--lg" to="docs/intro">
             Start Building →
           </Link>
         </div>
