69ea5d811f92c27984dd4517d5f2fb83f72cb38d
dineshpinto <annual.fallout_0z@icloud.com>
2024-06-22 19:13:32 +0400

    Fix workflow names of Python and Rust tests, bugfix Python tests

--- code_examples/developer-hub-python/test_queries.py
+++ code_examples/developer-hub-python/test_queries.py
@@ -35,7 +35,7 @@ async def test_fetch_abi_flare():
 
 @pytest.mark.asyncio
 async def test_ftsov2_consumer_coston():
-    from ftsov2_consumer_coston import main
+    from ftsov2_consumer_coston2 import main
 
     feeds, decimals, timestamp = await main()
     assert len(feeds) == len(decimals)
