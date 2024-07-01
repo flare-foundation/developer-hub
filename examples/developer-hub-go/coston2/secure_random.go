c12d6aa00da08fb0130769e029d3ea421bc44c65
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-07-01 14:37:17 +0400

    feat: add js, rust and go guides for secure RNG

---
+++ examples/developer-hub-go/coston2/secure_random.go
@@ -0,0 +1,28 @@
+// THIS IS EXAMPLE CODE. DO NOT USE THIS CODE IN PRODUCTION.
+package coston2
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func SecureRandom() {
+	// Relay address (Flare Testnet Coston2)
+	address := common.HexToAddress("0x5CdF9eAF3EB8b44fB696984a1420B56A7575D250")
+	rpcUrl := "https://rpc.ankr.com/flare_coston2"
+	// Connect to an RPC node
+	client, _ := ethclient.Dial(rpcUrl)
+	// Set up contract instance
+	secureRng, _ := NewRelay(address, client)
+	// Fetch secure random number
+	opts := &bind.CallOpts{Context: context.Background()}
+	res, _ := secureRng.GetRandomNumber(opts)
+	// Print results
+	fmt.Println("Random number: ", res.RandomNumber)
+	fmt.Println("Is secure random: ", res.IsSecureRandom)
+	fmt.Println("Timestamp: ", res.RandomTimestamp)
+}
