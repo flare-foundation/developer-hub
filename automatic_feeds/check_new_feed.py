698367c33051c3edb0497f5364fb9659ee09d8cd
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-08-14 13:56:24 +0400

    fix: formatting issues with yml, linting issues with ruff

--- automatic_feeds/check_new_feed.py
+++ automatic_feeds/check_new_feed.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pycoingecko import CoinGeckoAPI
 
 CURRENT_FEEDS = [
@@ -30,28 +32,39 @@ title: "[auto_req]: New Feed"
 ---
 """
 
+
 def format_dict_to_str(coin_item: dict) -> str:
     newdict = {
         "name": coin_item["name"],
         "symbol": coin_item["symbol"],
         "coingecko_id": coin_item["id"],
-        "price_change_percentage_24h": coin_item["data"]["price_change_percentage_24h"]["usd"],
+        "price_change_percentage_24h": coin_item["data"]["price_change_percentage_24h"][
+            "usd"
+        ],
         "thumbnail": coin_item["thumb"],
-        "description": coin_item["data"]["content"]["description"] if coin_item["data"]["content"] else "",
-        "total_volume": coin_item["data"]["total_volume"]
+        "description": coin_item["data"]["content"]["description"]
+        if coin_item["data"]["content"]
+        else "",
+        "total_volume": coin_item["data"]["total_volume"],
     }
-    return '\n'.join("{}: {}".format(k, v) for k, v in newdict.items())
+    return "\n".join(f"{k}: {v}" for k, v in newdict.items())
+
 
 if __name__ == "__main__":
+    MAX_MARKET_CAP_RANK = 100
+
     cg = CoinGeckoAPI()
     trending = cg.get_search_trending()
 
-    with open("issues.md", "w") as f:
+    with Path.open("issues.md", "w") as f:
         f.write(header)
         selected_coin_data = []
         f.write("Coins that meet criteria:" + "\n\n")
         for coin in trending["coins"]:
-            if coin["item"]["market_cap_rank"] < 100 and coin["item"]["symbol"] not in CURRENT_FEEDS:
+            if (
+                coin["item"]["market_cap_rank"] < MAX_MARKET_CAP_RANK
+                and coin["item"]["symbol"] not in CURRENT_FEEDS
+            ):
                 item_data = coin["item"]
                 selected_coin_data.append(coin["item"])
                 f.write(f"## {coin["item"]["name"]}" + "\n")
