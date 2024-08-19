418be7d7c338a46b2e3c897a28534fafa87aa070
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-08-19 14:46:59 +0400

    chore: rename test to tests

--- examples/developer-hub-javascript/tests/secure_random_coston2_ethers.test.js
+++ examples/developer-hub-javascript/tests/secure_random_coston2_ethers.test.js
@@ -1,6 +1,7 @@
 import { main } from "../secure_random_coston2_ethers";
+
 describe("Fetch Random Number", () => {
-  test("should fetch  contarct address", async () => {
+  test("should fetch contract address", async () => {
     // Require the main function and await its execution
     const res = await main();
     expect(res.length).toEqual(3);
--- examples/developer-hub-javascript/tests/secure_random_coston2_web3.test.js
+++ examples/developer-hub-javascript/tests/secure_random_coston2_web3.test.js
@@ -1,6 +1,7 @@
 import { main } from "../secure_random_coston2_web3";
+
 describe("Fetch Random Number", () => {
-  test("should fetch  contarct address", async () => {
+  test("should fetch contract address", async () => {
     // Require the main function and await its execution
     const res = await main();
     expect(res).toEqual(
