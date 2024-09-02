8e1cf691509907bce1d08ddfc80cb53e11924186
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-09-02 22:53:02 +0400

    fix: reformat go code

--- examples/developer-hub-go/coston2/make_query.go
+++ examples/developer-hub-go/coston2/make_query.go
@@ -3,6 +3,7 @@ package coston2
 import (
 	"context"
 	"fmt"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
