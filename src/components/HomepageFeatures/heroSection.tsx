c126125f31bd2276a50131322a754d192287ea52
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-09-03 09:10:23 +0400

    fix: improve site accessibility

--- src/components/HomepageFeatures/heroSection.tsx
+++ src/components/HomepageFeatures/heroSection.tsx
@@ -57,7 +57,7 @@ export default function HeroSection() {
           srcSet="/img/landing/dev_hub_hero_mobile.webp"
           media="(max-width: 996px)"
         />
-        <img src="" />
+        <img src="" alt="Flare Developer Hub Landing Image" />
       </picture>
       <div className={clsx(classes.content, "container")}>
         <div className={classes.callToAction}>
