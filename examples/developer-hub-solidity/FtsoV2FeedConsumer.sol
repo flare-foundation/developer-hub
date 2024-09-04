5f59dc71de9f08878259a16e483d7701a17aa81f
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-09-04 21:54:59 +0400

    fix(ftso): preparation for FTSOv2Interface

--- examples/developer-hub-solidity/FtsoV2FeedConsumer.sol
+++ examples/developer-hub-solidity/FtsoV2FeedConsumer.sol
@@ -1,30 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import {IFlareContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/util-contracts/userInterfaces/IFlareContractRegistry.sol";
-import {IFastUpdater} from "@flarenetwork/flare-periphery-contracts/coston2/ftso/userInterfaces/IFastUpdater.sol";
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/flare/ContractRegistry.sol";
+import {FtsoV2Interface} from "@flarenetwork/flare-periphery-contracts/flare/FtsoV2Interface.sol";
 
 /**
  * THIS IS AN EXAMPLE CONTRACT.
  * DO NOT USE THIS CODE IN PRODUCTION.
  */
 contract FtsoV2FeedConsumer {
-    IFlareContractRegistry internal contractRegistry;
-    IFastUpdater internal ftsoV2;
+    FtsoV2Interface internal ftsoV2;
     // Feed indexes: 0 = FLR/USD, 2 = BTC/USD, 9 = ETH/USD
+    // See https://dev.flare.network/ftso/feeds for all feeds
     uint256[] public feedIndexes = [0, 2, 9];
 
     /**
      * Constructor initializes the FTSOv2 contract.
-     * The contract registry is used to fetch the FTSOv2 contract address.
+     * The contract registry is used to fetch the FtsoV2Interface contract address.
      */
     constructor() {
-        contractRegistry = IFlareContractRegistry(
-            0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019
-        );
-        ftsoV2 = IFastUpdater(
-            contractRegistry.getContractAddressByName("FastUpdater")
-        );
+        ftsoV2 = ContractRegistry.getFtsoV2();
     }
 
     /**
@@ -32,7 +27,7 @@ contract FtsoV2FeedConsumer {
      */
     function getFtsoV2CurrentFeedValues()
         external
-        view
+        payable
         returns (
             uint256[] memory _feedValues,
             int8[] memory _decimals,
@@ -43,7 +38,7 @@ contract FtsoV2FeedConsumer {
             uint256[] memory feedValues,
             int8[] memory decimals,
             uint64 timestamp
-        ) = ftsoV2.fetchCurrentFeeds(feedIndexes);
+        ) = ftsoV2.getFeedsByIndex(feedIndexes);
         /* Your custom feed consumption logic. In this example the values are just returned. */
         return (feedValues, decimals, timestamp);
     }
