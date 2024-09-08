7bc50c7037f6c8bec39bbfa10eea722064e146b8
Dinesh Pinto <annual.fallout_0z@icloud.com>
2024-09-08 11:39:50 +0400

    fix: update contracts for FtsoV2Interface

--- examples/developer-hub-solidity/FtsoV2FeedConsumer.sol
+++ examples/developer-hub-solidity/FtsoV2FeedConsumer.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/flare/ContractRegistry.sol";
-import {FtsoV2Interface} from "@flarenetwork/flare-periphery-contracts/flare/FtsoV2Interface.sol";
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
+import {FtsoV2Interface} from "@flarenetwork/flare-periphery-contracts/coston2/FtsoV2Interface.sol";
 
 /**
  * THIS IS AN EXAMPLE CONTRACT.
@@ -10,9 +10,12 @@ import {FtsoV2Interface} from "@flarenetwork/flare-periphery-contracts/flare/Fts
  */
 contract FtsoV2FeedConsumer {
     FtsoV2Interface internal ftsoV2;
-    // Feed indexes: 0 = FLR/USD, 2 = BTC/USD, 9 = ETH/USD
-    // See https://dev.flare.network/ftso/feeds for all feeds
-    uint256[] public feedIndexes = [0, 2, 9];
+    // Feed IDs, see https://dev.flare.network/ftso/feeds for full list
+    bytes21[] public feedIds = [
+        bytes21(0x01464c522f55534400000000000000000000000000), // FLR/USD
+        bytes21(0x014254432f55534400000000000000000000000000), // BTC/USD
+        bytes21(0x014554482f55534400000000000000000000000000) // ETH/USD
+    ];
 
     /**
      * Constructor initializes the FTSOv2 contract.
@@ -38,7 +41,7 @@ contract FtsoV2FeedConsumer {
             uint256[] memory feedValues,
             int8[] memory decimals,
             uint64 timestamp
-        ) = ftsoV2.getFeedsByIndex(feedIndexes);
+        ) = ftsoV2.getFeedsById(feedIds);
         /* Your custom feed consumption logic. In this example the values are just returned. */
         return (feedValues, decimals, timestamp);
     }
--- examples/developer-hub-solidity/SecureRandomConsumer.sol
+++ examples/developer-hub-solidity/SecureRandomConsumer.sol
@@ -1,30 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import {IFlareContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/util-contracts/userInterfaces/IFlareContractRegistry.sol";
-import {IRelay} from "@flarenetwork/flare-periphery-contracts/coston2/util-contracts/userInterfaces/IRelay.sol";
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
+import {RandomNumberV2Interface} from "@flarenetwork/flare-periphery-contracts/coston2/RandomNumberV2Interface.sol";
 
 /**
  * THIS IS AN EXAMPLE CONTRACT.
  * DO NOT USE THIS CODE IN PRODUCTION.
  */
 contract SecureRandomConsumer {
-    IFlareContractRegistry internal contractRegistry;
-    IRelay internal relay;
+    RandomNumberV2Interface internal randomV2;
 
     /**
-     * Constructor initializes the Relay contract, where the secure RNG is served.
-     * The contract registry is used to fetch the Relay contract address.
+     * Initializing an instance with RandomNumberV2Interface.
+     * The contract registry is used to fetch the contract address.
      */
     constructor() {
-        contractRegistry = IFlareContractRegistry(
-            0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019
-        );
-        relay = IRelay(contractRegistry.getContractAddressByName("Relay"));
+        randomV2 = ContractRegistry.getRandomNumberV2();
     }
 
     /**
-     * Fetch and check the latest secure random number.
+     * Fetch the latest secure random number.
      * The random number is generated every 90 seconds.
      */
     function getSecureRandomNumber()
@@ -32,7 +28,7 @@ contract SecureRandomConsumer {
         view
         returns (uint256 randomNumber, bool isSecure, uint256 timestamp)
     {
-        (randomNumber, isSecure, timestamp) = relay.getRandomNumber();
+        (randomNumber, isSecure, timestamp) = randomV2.getRandomNumber();
         /* DO NOT USE THE RANDOM NUMBER IF isSecure=false. */
         require(isSecure, "Random number is not secure");
         /* Your custom RNG consumption logic. In this example the values are just returned. */
