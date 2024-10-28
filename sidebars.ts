058672cd11f0a52bb0fc8cd8205d0c950ae16566
Steven Gregg <steven@flare.network>
2024-10-28 14:57:42 -0400

    Adding open beta page

--- sidebars.ts
+++ sidebars.ts
@@ -166,6 +166,7 @@ const sidebars: SidebarsConfig = {
       collapsed: true,
       link: { type: "doc", id: "fassets/overview" },
       items: [
+        "fassets/open-beta",
         // "fassets/getting-started",
         {
           type: "category",
