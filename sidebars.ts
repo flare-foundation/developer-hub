29c61ca191e2488ae2936418d06a6a04701e9edb
swarnabhasinha <swarnabhasinha@gmail.com>
2025-02-20 18:08:21 +0530

    feat(docs): new hackathon page and enhancement of the onboarding

--- sidebars.ts
+++ sidebars.ts
@@ -21,7 +21,7 @@ const sidebars: SidebarsConfig = {
         type: "doc",
         id: "hackathon/onboarding",
       },
-      items: ["hackathon/onboarding"],
+      items: ["hackathon/onboarding", "hackathon/VM-Setup"],
     },
     {
       type: "category",
