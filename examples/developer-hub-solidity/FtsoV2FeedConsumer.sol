3cf478dc88415c523fadf349fcb1913df2d0ecee
Dinesh Pinto <annual.fallout_0z@icloud.com>
2025-03-31 00:45:28 +0400

    fix(format): format solidity contracts

--- examples/developer-hub-solidity/FtsoV2FeedConsumer.sol
+++ examples/developer-hub-solidity/FtsoV2FeedConsumer.sol
@@ -13,21 +13,21 @@ contract FtsoV2FeedConsumer {
     TestFtsoV2Interface internal ftsoV2;
     // Feed IDs, see https://dev.flare.network/ftso/feeds for full list
     bytes21[] public feedIds = [
-    bytes21(0x01464c522f55534400000000000000000000000000), // FLR/USD
-    bytes21(0x014254432f55534400000000000000000000000000), // BTC/USD
-    bytes21(0x014554482f55534400000000000000000000000000) // ETH/USD
+        bytes21(0x01464c522f55534400000000000000000000000000), // FLR/USD
+        bytes21(0x014254432f55534400000000000000000000000000), // BTC/USD
+        bytes21(0x014554482f55534400000000000000000000000000) // ETH/USD
     ];
 
     /**
      * Get the current value of the feeds.
      */
     function getFtsoV2CurrentFeedValues()
-    external
-    returns (
-        uint256[] memory _feedValues,
-        int8[] memory _decimals,
-        uint64 _timestamp
-    )
+        external
+        returns (
+            uint256[] memory _feedValues,
+            int8[] memory _decimals,
+            uint64 _timestamp
+        )
     {
         /* THIS IS A TEST METHOD, in production use: ftsoV2 = ContractRegistry.getFtsoV2(); */
         ftsoV2 = ContractRegistry.getTestFtsoV2();
