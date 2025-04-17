092ae6a9db59ab60036a472be55319be7dd5c448
Kristaps Grinbergs <kristaps@flare.network>
2025-04-17 13:03:05 +0300

    refactor: improve code formatting and clarity in RandomNumber fetching examples in the Flare Contracts Registry guide

--- examples/developer-hub-solidity/RandomNumberFetcherViaRegistry.sol
+++ examples/developer-hub-solidity/RandomNumberFetcherViaRegistry.sol
@@ -1,22 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import { IFlareContractRegistry } from "@flarenetwork/flare-periphery-contracts/coston/IFlareContractRegistry.sol";
-import { RandomNumberV2Interface } from "@flarenetwork/flare-periphery-contracts/coston/RandomNumberV2Interface.sol";
+import {IFlareContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston/IFlareContractRegistry.sol";
+import {RandomNumberV2Interface} from "@flarenetwork/flare-periphery-contracts/coston/RandomNumberV2Interface.sol";
 
 contract RandomNumberFetcher {
     // Fetches the RandomNumberV2 contract from the registry by name
-    function getRandomNumberViaRegistryName() public view returns(
-        uint256 _randomNumber,
-        bool _isSecureRandom,
-        uint256 _randomTimestamp
-    ) {
+    function getRandomNumberViaRegistryName()
+        public
+        view
+        returns (
+            uint256 _randomNumber,
+            bool _isSecureRandom,
+            uint256 _randomTimestamp
+        )
+    {
         // Get the Flare Contracts Registry smart contract
-        IFlareContractRegistry flareContractRegistry = IFlareContractRegistry(0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019);
+        IFlareContractRegistry flareContractRegistry = IFlareContractRegistry(
+            0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019
+        );
         // Get the address of the Random Number V2 contract
-        address randomNumberV2Address = flareContractRegistry.getContractAddressByName("RandomNumberV2");
+        address randomNumberV2Address = flareContractRegistry
+            .getContractAddressByName("RandomNumberV2");
         // Define the random number V2 interface
-        RandomNumberV2Interface randomNumberV2 = RandomNumberV2Interface(randomNumberV2Address);
+        RandomNumberV2Interface randomNumberV2 = RandomNumberV2Interface(
+            randomNumberV2Address
+        );
 
         return randomNumberV2.getRandomNumber();
     }
