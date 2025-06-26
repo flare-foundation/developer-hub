6829a0358c6e29dff44907fe2f4bac6a6220c352
Kristaps Grinbergs <kristaps@flare.network>
2025-06-26 14:18:08 +0300

    refactor(docs): update FAssets redemption guide and contract to include FXRP approval process and new asset manager address

--- examples/developer-hub-solidity/FAssetsRedeem.sol
+++ examples/developer-hub-solidity/FAssetsRedeem.sol
@@ -1,18 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IAssetManager} from "@flarenetwork/flare-periphery-contracts/coston/IAssetManager.sol";
-import {AssetManagerSettings} from "@flarenetwork/flare-periphery-contracts/coston/userInterfaces/data/AssetManagerSettings.sol";
+import {IAssetManager} from "@flarenetwork/flare-periphery-contracts/coston2/IAssetManager.sol";
+import {AssetManagerSettings} from "@flarenetwork/flare-periphery-contracts/coston2/userInterfaces/data/AssetManagerSettings.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract FAssetsRedeem {
     // 1. Use the AssetManager contract
     IAssetManager public immutable assetManager;
+    IERC20 public immutable underlyingToken;
 
-    constructor(address _assetManager) {
+    address public immutable fAssetToken;
+
+    constructor(address _assetManager, address _fAssetToken) {
         assetManager = IAssetManager(_assetManager);
+        fAssetToken = _fAssetToken;
     }
 
-    // 2. Get the AssetManager settings
+    // 2. Approve FAssets for redemption
+    function approveFAssets(uint256 _amount) public returns (bool) {
+        return IERC20(fAssetToken).approve(address(this), _amount);
+    }
+
+    // 3. Redeem FAssets (requires prior approval)
+    function redeem(
+        uint256 _lots,
+        string memory _redeemerUnderlyingAddressString
+    ) public returns (uint256) {
+        // Calculate the amount of FXRP needed for redemption
+        AssetManagerSettings.Data memory settings = assetManager.getSettings();
+        uint256 amountToRedeem = settings.lotSizeAMG * _lots;
+
+        // Transfer FXRP from caller to AssetManager
+        IERC20(fAssetToken).transferFrom(
+            msg.sender,
+            address(this),
+            amountToRedeem
+        );
+
+        uint256 redeemedAmountUBA = assetManager.redeem(
+            _lots,
+            _redeemerUnderlyingAddressString,
+            payable(address(0))
+        );
+
+        return redeemedAmountUBA;
+    }
+
+    // 4. Get the AssetManager settings
     function getSettings()
         public
         view
@@ -23,21 +58,5 @@ contract FAssetsRedeem {
         assetDecimals = settings.assetDecimals;
 
         return (lotSizeAMG, assetDecimals);
-    }
-
-    // 3. Redeem FAssets
-    function redeem(
-        uint256 _lots,
-        string memory _redeemerUnderlyingAddressString
-    ) public returns (uint256) {
-        uint256 redeemedAmountUBA = assetManager.redeem(
-            _lots,
-            _redeemerUnderlyingAddressString,
-            // The account that is allowed to execute redemption default (besides redeemer and agent).
-            // In this case it is not used
-            payable(address(0))
-        );
-
-        return redeemedAmountUBA;
     }
 }
