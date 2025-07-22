a56766c0cfca44777c522e36001921b88295c9b9
Kristaps Grinbergs <kristaps@flare.network>
2025-07-22 17:00:17 +0300

    refactor(script): update asset manager address retrieval in getFXRP script

--- examples/developer-hub-javascript/fassets_get_fxrp.ts
+++ examples/developer-hub-javascript/fassets_get_fxrp.ts
@@ -1,14 +1,18 @@
+import { getFXRPAssetManagerAddress } from "../utils/fassets";
+
 // yarn hardhat run scripts/fassets/getFXRP.ts --network coston2
-
-// AssetManager address on Flare Testnet Coston2 network
-const ASSET_MANAGER_ADDRESS = "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5";
-
 const IAssetManager = artifacts.require("IAssetManager");
 
 async function main() {
-  const assetManager = await IAssetManager.at(ASSET_MANAGER_ADDRESS);
+  // 1. Connect to the AssetManager contract using the IAssetManager interface
+  const assetManager = await IAssetManager.at(
+    await getFXRPAssetManagerAddress(),
+  );
+
+  // 2. Get the FXRP token address
   const fasset = await assetManager.fAsset();
 
+  // 3. Log the FXRP token address
   console.log("FXRP address", fasset);
 }
 
