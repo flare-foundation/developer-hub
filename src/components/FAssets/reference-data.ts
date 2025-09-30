152fc6d4c2a188b5af1776f7c6a0acd5cf6211c8
Kristaps Grinbergs <kristaps@flare.network>
2025-09-30 13:57:30 +0300

    feat(reference-data): update asset addresses and add guides for FXRP asset manager

--- src/components/FAssets/reference-data.ts
+++ src/components/FAssets/reference-data.ts
@@ -7,8 +7,8 @@ export const reference = [
       coston: "AssetManagerController",
     },
     address: {
-      flare: "",
-      coston2: "0x0f575c6eC54176a5681cD5500fc770e9133b8982",
+      flare: "0x097B93eEBe9b76f2611e1E7D9665a9d7Ff5280B3",
+      coston2: "0x1C772F700308aF4c13897cc7b9c41EFfB82c50C0",
       songbird: "0x04025BBf6Be30d6cB6ad412c2958c712c6B2BCbA",
       coston: "0x572DeF121DC83332887E25e34aD51C2f5f40BC97",
     },
@@ -23,13 +23,17 @@ export const reference = [
       coston: "AssetManager Testnet XRP",
     },
     address: {
-      flare: "",
-      coston2: "0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5",
+      flare: "0x2a3Fe068cD92178554cabcf7c95ADf49B4B0B6A8",
+      coston2: "0xc1Ca88b937d0b528842F95d5731ffB586f4fbDFA",
       songbird: "0x299d678f67e7ADD4efdf295Ebe0E92FCb4f75C4c",
       coston: "0x56728e46908fB6FcC5BCD2cc0c0F9BB91C3e4D34",
     },
     description:
-      "Smart contract that can mint and burn FXRP while managing collateral and backing funds.",
+      "Smart contract that can mint and burn FXRP while managing collateral and backing funds. Added to the Flare contracts registry.",
+    guide: {
+      title: "Learn how to get the FXRP asset manager address.",
+      link: "/fassets/developer-guides/fassets-asset-manager-address-contracts-registry",
+    },
   },
   {
     name: {
@@ -39,11 +43,15 @@ export const reference = [
       coston: "FTestXRP",
     },
     address: {
-      flare: "",
-      coston2: "0x8b4abA9C4BD7DD961659b02129beE20c6286e17F",
+      flare: "0xAd552A648C74D49E10027AB8a618A3ad4901c5bE",
+      coston2: "0x0b6A3645c240605887a5532109323A3E12273dc7",
       songbird: "0xF9a84f4ec903F4EaB117A9c1098BeC078BA7027d",
       coston: "0x36be8f2e1CC3339Cf6702CEfA69626271C36E2fd",
     },
-    description: "The FAsset-wrapped XRP token, ready for use on Songbird.",
+    description: "The FAsset-wrapped XRP token.",
+    guide: {
+      title: "Learn how to get the FXRP address.",
+      link: "/fassets/developer-guides/fassets-fxrp-address",
+    },
   },
 ];
