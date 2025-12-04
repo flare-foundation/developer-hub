02da995371e84aff4ffba0e1a206477ade9e2243
Dinesh Pinto <annual.fallout_0z@icloud.com>
2025-12-04 11:44:00 +0400

    fix(perf): lazy load swagger

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,16 @@ const config: Config = {
   // Experimental features in preparation for Docusaurus v4 upgrade
   future: {
     v4: true,
-    experimental_faster: true,
+    experimental_faster: {
+      swcJsLoader: true,
+      swcJsMinimizer: true,
+      swcHtmlMinimizer: true,
+      lightningCssMinimizer: true,
+      rspackBundler: false,
+      rspackPersistentCache: false,
+      ssgWorkerThreads: true,
+      mdxCrossCompilerCache: true,
+    },
   },
 
   presets: [
